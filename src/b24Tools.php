0417366ce88a2b46c5ab97d05a3c7cfb58c14425
Luk <33396443+lukoyanov-aa@users.noreply.github.com>
2022-11-24 18:49:31 +0300

    fix: Возможность переустановки приложения, исправлен путь логирования, Добавлена обработка смены имени портала.

--- src/b24Tools.php
+++ src/b24Tools.php
@@ -2,11 +2,11 @@
 
 namespace wm\b24tools;
 
-use yii\base\BaseObject;
-use Yii;
-use wm\admin\models\User;
-use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use wm\admin\models\User;
+use Yii;
+use yii\base\BaseObject;
 use yii\helpers\ArrayHelper;
 use yii\web\HttpException;
 
@@ -15,7 +15,8 @@ use yii\web\HttpException;
  *
  * @author Админ
  */
-class b24Tools extends \yii\base\BaseObject {
+class b24Tools extends \yii\base\BaseObject
+{
 
     /**
      * @var
@@ -46,20 +47,31 @@ class b24Tools extends \yii\base\BaseObject {
      */
     private $applicationSecret;
 
+    /**
+     * @var
+     */
+    private $onPortalRenamed;
+
 //    public function __construct($config = array()) {
 //        $this->b24PortalTable = Yii::$app->params['b24PortalTable'];
 //        parent::__construct($config);
 //    }
+
+    public function setOnPortalRenamed($onPortalRenamed)
+    {
+        $this->onPortalRenamed = $onPortalRenamed;
+    }
 
     /**
      * @param $domain
      * @return array|false|\yii\db\DataReader
      * @throws \yii\db\Exception
      */
-    private function getAuthFromDB($domain) {
+    private function getAuthFromDB($domain)
+    {
         $res = Yii::$app->db
-                ->createCommand("SELECT * FROM " . $this->b24PortalTable . " WHERE PORTAL = '" . $domain . "'")
-                ->queryOne();
+            ->createCommand("SELECT * FROM " . $this->b24PortalTable . " WHERE PORTAL = '" . $domain . "'")
+            ->queryOne();
         return $res;
     }
 
@@ -70,18 +82,42 @@ class b24Tools extends \yii\base\BaseObject {
      * @return int
      * @throws \yii\db\Exception
      */
-    public function addAuthToDB($tableName, $auth) {
-        $res = Yii::$app->db
+    public function addAuthToDB($tableName, $auth)
+    {
+        //TODO: Подумать над использованием моделей
+        $item = (new \yii\db\Query())
+            ->select('PORTAL')
+            ->from($tableName)
+            ->where([
+                'PORTAL' => $auth['domain']
+            ])
+            ->one();
+
+        if ($item) {
+            $res = Yii::$app->db
                 ->createCommand()
-                ->insert($tableName, [
-                    'PORTAL' => $auth['domain'],
+                ->update($tableName, [
                     'ACCESS_TOKEN' => $auth['access_token'],
                     'REFRESH_TOKEN' => $auth['refresh_token'],
                     'MEMBER_ID' => $auth['member_id'],
                     'DATE' => date("Y-m-d"),
-                        ]
+                ],
+                    ['PORTAL' => $auth['domain']]
                 )
                 ->execute();
+        } else {
+            $res = Yii::$app->db
+                ->createCommand()
+                ->insert($tableName, [
+                        'PORTAL' => $auth['domain'],
+                        'ACCESS_TOKEN' => $auth['access_token'],
+                        'REFRESH_TOKEN' => $auth['refresh_token'],
+                        'MEMBER_ID' => $auth['member_id'],
+                        'DATE' => date("Y-m-d"),
+                    ]
+                )
+                ->execute();
+        }
         return $res;
     }
 
@@ -91,18 +127,19 @@ class b24Tools extends \yii\base\BaseObject {
      * @return int
      * @throws \yii\db\Exception
      */
-    public function updateAuthToDB($auth) {
+    public function updateAuthToDB($auth)
+    {
         if ($this->b24PortalTable) {
             $res = Yii::$app->db
-                    ->createCommand()
-                    ->update($this->b24PortalTable, [
-                        'ACCESS_TOKEN' => $auth['access_token'],
-                        'REFRESH_TOKEN' => $auth['refresh_token'],
-                        'DATE' => date("Y-m-d"),
-                            ], ['PORTAL' => $auth['domain'],
+                ->createCommand()
+                ->update($this->b24PortalTable, [
+                    'ACCESS_TOKEN' => $auth['access_token'],
+                    'REFRESH_TOKEN' => $auth['refresh_token'],
+                    'DATE' => date("Y-m-d"),
+                ], ['PORTAL' => $auth['domain'],
                         'MEMBER_ID' => $auth['member_id'],]
-                    )
-                    ->execute();
+                )
+                ->execute();
             return $res;
         }
     }
@@ -111,7 +148,8 @@ class b24Tools extends \yii\base\BaseObject {
      * @param $arAccessParams
      * @return array
      */
-    private function prepareFromDB($arAccessParams) {
+    private function prepareFromDB($arAccessParams)
+    {
         $arResult = array();
         $arResult['domain'] = $arAccessParams['PORTAL'];
         $arResult['member_id'] = $arAccessParams['MEMBER_ID'];
@@ -125,7 +163,8 @@ class b24Tools extends \yii\base\BaseObject {
      * @param $arRequest
      * @return array
      */
-    public function prepareFromAjaxRequest($arRequest) {
+    public function prepareFromAjaxRequest($arRequest)
+    {
         $arResult = array();
         $arResult['domain'] = $arRequest['domain'];
         $arResult['member_id'] = $arRequest['member_id'];
@@ -140,7 +179,8 @@ class b24Tools extends \yii\base\BaseObject {
      * @param $arRequest
      * @return array
      */
-    public function prepareFromHandlerRequest($arRequest) {
+    public function prepareFromHandlerRequest($arRequest)
+    {
         $arResult = array();
         $arResult['domain'] = $arRequest['domain'];
         $arResult['member_id'] = $arRequest['member_id'];
@@ -155,8 +195,9 @@ class b24Tools extends \yii\base\BaseObject {
      * @param null $arRequestGet
      * @return array
      */
-    public function prepareFromRequest($arRequestPost = null, $arRequestGet = null) {
-        if (!$arRequestPost or ! $arRequestGet) {
+    public function prepareFromRequest($arRequestPost = null, $arRequestGet = null)
+    {
+        if (!$arRequestPost or !$arRequestGet) {
             return array();
         }
         $arResult = array();
@@ -174,7 +215,8 @@ class b24Tools extends \yii\base\BaseObject {
      * @return bool
      * @throws \yii\db\Exception
      */
-    public function checkB24Auth($arScope = array()) {
+    public function checkB24Auth($arScope = array())
+    {
 
         if (!is_array($arScope)) {
             $arScope = array();
@@ -188,7 +230,7 @@ class b24Tools extends \yii\base\BaseObject {
 
         // $arAccessParams['access_token'] = '123';
         // $arAccessParams['refresh_token'] = '333';
-        $this->arB24App = $this->getBitrix24($this->arAccessParams, $isTokenRefreshed, $this->b24_error, $arScope);
+        $this->arB24App = $this->getBitrix24($this->arAccessParams, $isTokenRefreshed, $this->b24_error, $arScope, $this->onPortalRenamed);
         if ($isTokenRefreshed and $this->b24PortalTable) {
             $this->updateAuthToDB($this->arAccessParams);
         }
@@ -204,9 +246,14 @@ class b24Tools extends \yii\base\BaseObject {
      * @throws \Bitrix24\Exceptions\Bitrix24Exception
      * @throws \yii\db\Exception
      */
-    private function getBitrix24(&$arAccessData, &$btokenRefreshed, &$errorMessage, $arScope = array()) {
+    private function getBitrix24(&$arAccessData, &$btokenRefreshed, &$errorMessage, $arScope = array(), $onPortalRenamed = null)
+    {
         $log = new Logger('bitrix24');
-        $log->pushHandler(new StreamHandler('log/b24/' . date('Y_m_d') . '.log', Logger::DEBUG));
+        $log = new Logger('bitrix24');
+        $loggerStream = '../log/' . date('Y_m_d') . '/' . $arAccessData['domain'] . '.log';
+        $isLoggingInfo = in_array($arAccessData['domain'], Yii::$app->params['extendedLoggerPortal']);
+        $loggerLevel = ($isLoggingInfo) ? Logger::INFO : Logger::DEBUG;
+        $log->pushHandler(new StreamHandler($loggerStream, $loggerLevel));
 
         $btokenRefreshed = null;
 
@@ -220,6 +267,10 @@ class b24Tools extends \yii\base\BaseObject {
         $obB24App->setApplicationScope($arScope);
         $obB24App->setApplicationId($this->applicationId);
         $obB24App->setApplicationSecret($this->applicationSecret);
+        if ($onPortalRenamed)
+        {
+            $obB24App->setOnPortalRenamed($onPortalRenamed);
+        }
 
         // set user-specific settings
         $obB24App->setDomain($arAccessData['domain']);
@@ -274,7 +325,8 @@ class b24Tools extends \yii\base\BaseObject {
      * @return false
      * @throws \yii\db\Exception
      */
-    public function connect($applicationId, $applicationSecret, $tableName = '', $domain = null, $arScope = array(), $autch = null) {
+    public function connect($applicationId, $applicationSecret, $tableName = '', $domain = null, $arScope = array(), $autch = null)
+    {
         $this->applicationId = $applicationId;
         $this->applicationSecret = $applicationSecret;
         $this->b24PortalTable = $tableName;
@@ -302,8 +354,9 @@ class b24Tools extends \yii\base\BaseObject {
      * @return false
      * @throws \yii\db\Exception
      */
-    public function connectFromUser($auth){
-        $b24App =$this->connect(
+    public function connectFromUser($auth)
+    {
+        $b24App = $this->connect(
             Yii::$app->params['applicationId'],
             Yii::$app->params['applicationSecret'],
             null,
@@ -317,7 +370,8 @@ class b24Tools extends \yii\base\BaseObject {
      * @return false
      * @throws \yii\db\Exception
      */
-    public function connectFromAdmin(){
+    public function connectFromAdmin()
+    {
         $userId = \Yii::$app->user->id;
         $portalName = User::getPortalName($userId);
         $b24App = $this->connect(
@@ -332,7 +386,8 @@ class b24Tools extends \yii\base\BaseObject {
      * @param $data
      * @return string
      */
-    public static function toBool($data) {
+    public static function toBool($data)
+    {
         return $data ? 'Y' : 'N';
     }
 
@@ -341,11 +396,12 @@ class b24Tools extends \yii\base\BaseObject {
      * @return bool
      * @throws HttpException
      */
-    public static function isEventOnline($data) {
+    public static function isEventOnline($data)
+    {
         if (!ArrayHelper::keyExists('offline', $data, false)) {
             throw new HttpException(404, 'Data "offline" not found');
         }
-        return !(bool) ArrayHelper::getValue($data, 'offline');
+        return !(bool)ArrayHelper::getValue($data, 'offline');
     }
 
     /**
@@ -353,11 +409,12 @@ class b24Tools extends \yii\base\BaseObject {
      * @return bool
      * @throws HttpException
      */
-    public static function isEventOffline($data) {
+    public static function isEventOffline($data)
+    {
         if (!ArrayHelper::keyExists('offline', $data, false)) {
             throw new HttpException(404, 'Data "offline" not found');
         }
-        return (bool) ArrayHelper::getValue($data, 'offline');
+        return (bool)ArrayHelper::getValue($data, 'offline');
     }
 
 }
